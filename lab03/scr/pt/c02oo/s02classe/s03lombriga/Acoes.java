6e31560a9b96a5795636c1dc66e7009c3b695f5a
TheDeas343 <82724293+TheDeas343@users.noreply.github.com>
2022-04-06 20:53:16 -0300

    Classe de execução das ações

--- lab03/scr/pt/c02oo/s02classe/s03lombriga/Acoes.java
+++ lab03/scr/pt/c02oo/s02classe/s03lombriga/Acoes.java
@@ -1,32 +1,32 @@
 package src.pt.c02oo.so2classe.s03lombriga;
 
-public class Acoes {
-	public class Acoes{
-		   String Acao;
-		   AquarioLombriga Lombriga;
-		 
-		   Acoes(String Acao){
-		       this.Acao = Acao;
-		     
-		       int AA = (Acao.charAt(0)-'0')*10 + (Acao.charAt(1)-'0');
-		       int LL = (Acao.charAt(2)-'0')*10 + (Acao.charAt(3)-'0');
-		       int PP = (Acao.charAt(4)-'0')*10 + (Acao.charAt(5)-'0');
-		     
-		       Lombriga = new AquarioLombriga( AA,  LL, PP);
-		   }
-		   public void executar(){
-		 
-		       Lombriga.apresenta();
-		       for (int i = 6; i < Acao.length();i++){
-		           if(Acao.charAt(i) == 'C')
-		               Lombriga.crescer();
-		           else if(Acao.charAt(i) == 'M')
-		               Lombriga.mover();
-		           else   
-		               Lombriga.virar();
-		           Lombriga.apresenta();
-		       }
-		   }
-		}
+//Classe que cria uma Lomnbriga no Aquario e executa as acoes requeridas
+public class Acoes{
+	   String Acao;
+	   AquarioLombriga Lombriga;
+	 
+	   Acoes(String Acao){
+	       this.Acao = Acao;
+	     
+	       int AA = (Acao.charAt(0)-'0')*10 + (Acao.charAt(1)-'0');
+	       int LL = (Acao.charAt(2)-'0')*10 + (Acao.charAt(3)-'0');
+	       int PP = (Acao.charAt(4)-'0')*10 + (Acao.charAt(5)-'0');
+	     
+	       Lombriga = new AquarioLombriga( AA,  LL, PP);
+	   }
+	   public void executar(){
+	 
+	       Lombriga.apresenta();
+	       for (int i = 6; i < Acao.length();i++){
+	           if(Acao.charAt(i) == 'C')
+	               Lombriga.crescer();
+	           else if(Acao.charAt(i) == 'M')
+	               Lombriga.mover();
+	           else   
+	               Lombriga.virar();
+	           Lombriga.apresenta();
+	       }
+	   }
+	}
+	
 
-}
