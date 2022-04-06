d99edf5ea32c3fe53b0b1a81d3b1fbdb78c35aac
TheDeas343 <82724293+TheDeas343@users.noreply.github.com>
2022-04-06 20:56:06 -0300

    Classe AppLab03 / Main

--- lab03/scr/pt/c02oo/s02classe/s03lombriga/AppLab03.java
+++ lab03/scr/pt/c02oo/s02classe/s03lombriga/AppLab03.java
@@ -1,6 +1,7 @@
 package src.pt.c02oo.so2classe.s03lombriga;
 import java.util.Scanner;
 
+//Classe principal de execucao do código
 public class AppLab03 {
 	public static void main(String[] args) {
 		
